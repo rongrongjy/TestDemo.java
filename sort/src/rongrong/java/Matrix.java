ad2b2ed7d939ac6eacf0bdf533bd24abf2df5800
rongrong <lurongrong_2020@163.com>
2021-06-13 13:43:49 +0800

    旋转矩阵

---
+++ sort/src/rongrong/java/Matrix.java
@@ -0,0 +1,60 @@
+package rongrong.java;
+
+/**
+ * @description：给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+ * 不占用额外内存空间能否做到？
+ * 给定 matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ *
+ * @auther lurongrong
+ * @create 2021-06-13 13:23
+ */
+public class Matrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+//                swap(matrix[i][j], matrix[n - i - 1][j]);
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+//                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+    public static void swap(int a,int b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
